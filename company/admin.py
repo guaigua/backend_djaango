ea9de2eb66429eda7d92a31fb063f162b7d31a30
German Guaigua <gerguaigua@gmail.com>
2022-08-16 11:33:26 -0300

    Feat(Company) Implementation of company

---
+++ company/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+# My models
+from company.models import Company
+
+admin.site.register(Company)
--- config/settings/base.py
+++ config/settings/base.py
@@ -56,6 +56,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
+   'company',
    'client',
    'utils'
 ]
