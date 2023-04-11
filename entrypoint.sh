22ca619bade6230fa2088a7ececefc2eb6ce360a
guaigua <gerguaigua@gmail.com>
2023-04-11 17:14:17 -0300

    Feat :recycle: Modify Configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,12 @@ echo "${0}: running migrations."
 python manage.py makemigrations --merge
 python manage.py migrate --noinput
 
-echo "${0}: collecting statics."
 
-python manage.py collectstatic --noinput
 
-cp -rv static/* static_shared/
-
-gunicorn backend_django.wsgi:application \
-    --env DJANGO_SETTINGS_MODULE=backend_django.production_settings \
-    --name backend_django \
-    --bind 0.0.0.0:8000 \
+gunicorn apps.wsgi:application \
+    --env DJANGO_SETTINGS_MODULE=apps.production_settings \
+    --name apps \
+    --bind 0.0.0.0:8001 \
     --timeout 600 \
     --workers 4 \
     --log-level=info \
