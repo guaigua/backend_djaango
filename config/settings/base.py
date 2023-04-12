3e41fba2dd9fc554f5c52a0600a91c367421a480
guaigua <gerguaigua@gmail.com>
2023-04-12 09:54:19 -0300

    Fix :beetle: Optimization with gunicorn

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,6 +21,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -159,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,19 @@
 
 set -e
 
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
 echo "${0}: running migrations."
 python manage.py makemigrations --merge
 python manage.py migrate --noinput
 
 
 
-gunicorn apps.wsgi:application \
-    --env DJANGO_SETTINGS_MODULE=apps.production_settings \
-    --name apps \
-    --bind 0.0.0.0:8001 \
+gunicorn config.wsgi:application \
+    --env DJANGO_SETTINGS_MODULE=config.settings.dev \
+    --name backend_django \
+    --bind 0.0.0.0:8000 \
     --timeout 600 \
     --workers 4 \
     --log-level=info \
