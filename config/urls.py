1b9ffdfc11c97e7302ffbcc753861d38e11416e7
German Guaigua <gerguaigua@gmail.com>
2023-04-21 19:29:07 -0300

    Fix :beetle: Static files

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Code made to generate Swagger documentation automatically through drf_yasg
 schema_view = get_schema_view(
@@ -46,3 +47,6 @@ urlpatterns = [
         schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
